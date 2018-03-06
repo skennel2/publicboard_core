024a6a0453624cc49ffefffb1c8055b1f37f8791
skennel2 <skennel2@gmail.com>
2018-03-06 06:08:08 -0800

    정리

--- src/main/java/org/almansa/app/service/postService/PostService.java
+++ src/main/java/org/almansa/app/service/postService/PostService.java
@@ -4,15 +4,18 @@ import java.util.Date;
 import java.util.List;
 
 import org.almansa.app.core.OwnerBoardInfomation;
+import org.almansa.app.core.OwnerBoardInfomationImpl;
 import org.almansa.app.core.WriterInfomation;
+import org.almansa.app.core.WriterInfomationImpl;
 import org.almansa.app.core.board.Board;
 import org.almansa.app.core.member.Member;
 import org.almansa.app.core.post.DefaultTextPost;
 import org.almansa.app.core.post.Post;
+import org.almansa.app.core.service.ServiceBase;
 import org.almansa.app.service.boardService.BoardRepository;
 import org.almansa.app.service.memberService.MemberRepository;
 
-public class PostService{
+public class PostService extends ServiceBase{
 	
 	private PostRepository postRepo;
 	private MemberRepository memberRepo;
@@ -21,65 +24,26 @@ public class PostService{
     final int postTextContentsMaximumLength = 300;
     final int postTextNameMaximumLength = 30;
 	
-	public void writeNewPost(long memberId, long boardId, String name, String contents) {
+	public void writeNewPost(final long memberId, final  long boardId, final String name, final String contents) {
 		final Member member = memberRepo.getById(memberId);			
 		final Board board = boardRepo.getById(boardId);
 		
-		verifyMember(member);
-		verifyBoard(board);	
+		verifyNotNull(member);
+		verifyNotNull(board);	
+		
+		OwnerBoardInfomationImpl boardInfo = new OwnerBoardInfomationImpl(board.getId());
+		WriterInfomationImpl writerInfo = new WriterInfomationImpl(member.getId(), member.loginId());
 		
 		Post post = new DefaultTextPost(
 				name, 
 				new Date(), 
 				new Date(), 
 				contents, 
-				ownerBoardInfomation(board.getId()), 
-				writerInfomation(member.getId(), member.loginId()), 
+				boardInfo, 
+				writerInfo, 
 				postTextContentsMaximumLength, 
 				postTextNameMaximumLength);
 		
 		postRepo.update(post);			
-	}
-	
-	private void verifyMember(Member member) {
-		if(member == null) {
-			throw new RuntimeException();
-		}
-	}
-	
-	private void verifyBoard(Board board) {
-		if(board == null) {
-			throw new RuntimeException();
-		}
-	}	
-	
-	private OwnerBoardInfomation ownerBoardInfomation(final long boardId) {
-		
-		OwnerBoardInfomation ownerBoardInfomation = new OwnerBoardInfomation() {
-			
-			@Override
-			public long getOwnerBoardKey() {
-				return boardId;
-			}
-		};
-		
-		return ownerBoardInfomation;
-	}
-	
-	private WriterInfomation writerInfomation(final long writerId, final String writerLoginId) {
-		WriterInfomation writerInfomation = new WriterInfomation() {
-			
-			@Override
-			public String getWriterLoginId() {
-				return writerLoginId;
-			}
-			
-			@Override
-			public long getWriterId() {
-				return writerId;
-			}
-		};
-		
-		return writerInfomation;
 	}
 }
