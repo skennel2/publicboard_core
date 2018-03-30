3e1720f853d59e834df1c3e138a8459d770328c9
나윤수 <skennel2@gmail.com>
2018-03-30 15:38:41 +0900

    댓글 출력

--- src/main/java/org/almansa/app/core/service/comment/CommentServiceImpl.java
+++ src/main/java/org/almansa/app/core/service/comment/CommentServiceImpl.java
@@ -16,7 +16,9 @@ import org.almansa.app.core.repository.member.MemberRepository;
 import org.almansa.app.core.repository.post.PostRepository;
 import org.almansa.app.core.service.ServiceBase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CommentServiceImpl extends ServiceBase implements CommentService{
     
     private MemberRepository memberRepo;
