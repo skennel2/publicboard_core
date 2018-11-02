85de58869589b43c6fcda33a387fc355d03d1e8e
skennel2 <skennel2@gmail.com>
2018-11-02 09:15:16 +0900

    util test

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Objects;
+
 import org.junit.Test;
 import org.springframework.util.Assert;
 
@@ -6,5 +10,21 @@ public class UtilTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void assertNotNullTest() {		
 		Assert.notNull(null, "no null");
+	}
+	
+	@Test
+	public void objectNonNull() {		
+		Boolean result = Objects.nonNull("asdf");
+		
+		assertEquals(true, result);
+		
+		result = Objects.nonNull(null);
+		
+		assertEquals(false, result);
+		
+		result = Objects.nonNull("");
+		
+		assertEquals(true, result);		
+		
 	}
 }
